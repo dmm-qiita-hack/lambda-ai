1193526b41646d51f181e704f90f38f76a34454e
TakatsuNobuhiro <nobuhiro19980622@gmail.com>
2024-02-10 20:12:44 +0900

    delete 無駄なprint消す消す

--- ai/main.py
+++ ai/main.py
@@ -2,19 +2,19 @@ import json
 import traceback
 from langchain.chat_models import AzureChatOpenAI
 from langchain.memory.chat_message_histories import DynamoDBChatMessageHistory
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
 chat = AzureChatOpenAI(openai_api_version="2023-05-15", deployment_name="takatsu")
 
-def chat_with_bot(session_id: str, message: str):
+def chat_with_bot(session_id: str, message: str, profile: str):
     chat_history = DynamoDBChatMessageHistory(table_name="qiita-ai", session_id=session_id)
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", chat_memory=chat_history, return_messages=True
+    memory = ConversationBufferWindowMemory(
+        memory_key="chat_history", chat_memory=chat_history, return_messages=True, k=3
     )
     prompt = PromptTemplate(
-        input_variables=["chat_history","Query"],
+        input_variables=["chat_history","Query", "profile"],
         template="""
         あなたはエンジニアマネージャーです。
         駆け出しエンジニアと会話してください。
@@ -23,10 +23,17 @@ def chat_with_bot(session_id: str, message: str):
         ```チャット履歴
         {chat_history}
         ```
-        Human: {Query}
-    Chatbot:
+
+        ```駆け出しエンジニアのプロフィール
+        {profile}
+        ```
+
+        ```駆け出しエンジニアの発言
+        {Query}
+        ```
     """
     )
+    prompt.format(profile=profile)
 
     llm_chain = LLMChain(
         llm=chat,
@@ -35,6 +42,19 @@ def chat_with_bot(session_id: str, message: str):
         memory=memory,
     )
     return llm_chain.predict(Query=message)
+
+def make_profile(username: str, note: str, proffesional_skill: str):
+    profile =  f"""
+    名前: {username} さん
+    """
+    if note is not None:
+        profile += f"自己紹介: {note}\n"
+    if proffesional_skill is not None:
+        profile += f"職種(スキル): {proffesional_skill}\n"
+
+
+
+    return profile
 
 
 def handler(event, context):
@@ -45,9 +65,14 @@ def handler(event, context):
         message = event.get('queryStringParameters', {}).get('message', None)
         if message is None:
             return {'statusCode': 400, 'body': json.dumps('message is required.')}
+        username = event.get('queryStringParameters', {}).get('username', None)
+        if username is None:
+            return {'statusCode': 400, 'body': json.dumps('username is required.')}
+        note = event.get('queryStringParameters', {}).get('note', None)
+        proffesional_skill = event.get('queryStringParameters', {}).get('proffesional_skill', None)
+        profile = make_profile(username, note, proffesional_skill)
         
-        result = chat_with_bot(session_id=session_id, message=message)
-        print(result)
+        result = chat_with_bot(session_id=session_id, message=message, profile=profile)
         return {'statusCode': 200, 'body': json.dumps({'message': result})}
     
     # エラーが起きた場合
